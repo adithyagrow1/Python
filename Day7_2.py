a0fad5e2e4e5b07539cc0ba9a5e0a9ed7538b36f
adithyagrow1 <adithya030105@outlook.com>
2026-01-18 18:05:51 +0530

    D7.py

---
+++ Day7_2.py
@@ -0,0 +1,8 @@
+response = input("would u like to eat some food!:")
+
+if response== "yes":
+    print("your food is being prepared")
+elif response== "no":
+    print("alright..")
+else:
+    print("no food for you")
