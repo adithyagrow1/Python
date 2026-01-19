33bda624bbceda755546249b9c769af3289dfcc2
adithyagrow1 <adithya030105@outlook.com>
2026-01-19 20:25:29 +0530

    D8.py

---
+++ Day8_4.py
@@ -0,0 +1,11 @@
+# And logical operator
+
+temp = 200
+is_sunny = True
+
+if temp <= 25 and is_sunny:
+    print("all set to play..!")
+elif temp>100 and is_sunny:
+    print("you'll are cooked")
+else:
+    print("game has been postponed;")
