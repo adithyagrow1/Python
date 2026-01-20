1aa420c96aa9e4226323740251914ffbf41cd0c4
adithyagrow1 <adithya030105@outlook.com>
2026-01-20 21:11:23 +0530

    D9.py

---
+++ Day9_2.py
@@ -0,0 +1,14 @@
+#strings
+
+name = input("Enter your full name:")
+#result = len(name)# length
+#result = name.find("h") # finds the first occurrence of the string
+#it returns -1 if it cant find any string
+#name = name.upper()# uppercase
+#name = name.lower()#Lowercase
+#name = name.isdigit() # returns true or false returns true only when the input is only digit
+#name = name.isalpha()
+name = name.replace("a","m")
+print(name)
+
+print(help(str))
