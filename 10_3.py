74598daceb03634ea21856ecb8f2a80baf9c119c
adithyagrow1 <adithya030105@outlook.com>
2026-01-21 22:08:51 +0530

    10.py

---
+++ 10_3.py
@@ -0,0 +1,13 @@
+text = input("Enter text: ")
+
+has_digit = False
+
+for char in text:
+    if char.isdigit():
+        has_digit = True
+        break
+
+if has_digit:
+    print("Contains number")
+else:
+    print("Only letters")
