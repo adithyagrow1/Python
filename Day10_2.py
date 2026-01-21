87b66248c77aa44594b7050299d14fb833318343
adithyagrow1 <adithya030105@outlook.com>
2026-01-21 22:07:49 +0530

    10.py

---
+++ Day10_2.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ").strip()
+first = name[0].upper()
+
+if first == "A":
+    print("Awesome name!")
+elif first == "M":
+    print("Magnificent name!")
+else:
+    print("Nice name!")
