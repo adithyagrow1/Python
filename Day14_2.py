ead73ec481f2c95d5eca95c1033359b8780987af
adithyagrow1 <adithya030105@outlook.com>
2026-01-26 21:52:06 +0530

    D14.py

---
+++ Day14_2.py
@@ -0,0 +1,29 @@
+destination = input("Enter destination (Delhi/Mumbai/Bangalore): ").lower()
+class_type = input("Enter class (economy/business/first): ").lower()
+
+if destination == "delhi":
+    if class_type == "economy":
+        print("Ticket Price: ₹3,500")
+    elif class_type == "business":
+        print("Ticket Price: ₹7,000")
+    else:
+        print("Ticket Price: ₹12,000")
+
+elif destination == "mumbai":
+    if class_type == "economy":
+        print("Ticket Price: ₹4,000")
+    elif class_type == "business":
+        print("Ticket Price: ₹8,000")
+    else:
+        print("Ticket Price: ₹13,000")
+
+elif destination == "bangalore":
+    if class_type == "economy":
+        print("Ticket Price: ₹3,000")
+    elif class_type == "business":
+        print("Ticket Price: ₹6,500")
+    else:
+        print("Ticket Price: ₹11,000")
+
+else:
+    print("Destination not available")
