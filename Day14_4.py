1cc85d0b51dc097643a644bde0d4b5ba68fe5ea6
adithyagrow1 <adithya030105@outlook.com>
2026-01-26 21:55:57 +0530

    D14.py

--- Day14_4.py
+++ Day14_4.py
@@ -3,6 +3,8 @@ text = input("Enter a sentence: ").lower()
 bad_words = ["idiot", "stupid", "dumb"]
 
 if any(bad in text for bad in bad_words):
+
     print("Warning: Your message contains inappropriate words.")
 else:
+    
     print("Message is clean.")
