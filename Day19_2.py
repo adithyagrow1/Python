2068d76b12339d28b75b378a9931a79c143691dd
adithyagrow1 <adithya030105@outlook.com>
2026-01-31 21:49:40 +0530

    D19.py

---
+++ Day19_2.py
@@ -0,0 +1,19 @@
+review = input("Enter your movie review: ").lower()
+
+positive_words = ["good", "great", "excellent", "amazing", "superb"]
+negative_words = ["bad", "boring", "worst", "poor", "terrible"]
+
+pos = any(word in review for word in positive_words)
+neg = any(word in review for word in negative_words)
+
+if pos and not neg:
+    print("Sentiment: 👍 Positive Review")
+
+elif neg and not pos:
+    print("Sentiment: 👎 Negative Review")
+
+elif pos and neg:
+    print("Sentiment: 😐 Mixed Review")
+
+else:
+    print("Sentiment: 🤔 Neutral / Not Clear")
