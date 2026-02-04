6d430f1e318ff7da0ff8875cf6dc82ec7849fa89
adithyagrow1 <adithya030105@outlook.com>
2026-02-04 22:44:50 +0530

    D22.py

--- Day22_2.py
+++ Day22_2.py
@@ -1,9 +1,14 @@
+
+
 goal = input("What is your fitness goal? ").lower()
 
 if "weight loss" in goal or "lose weight" in goal:
     print("Plan: Cardio + calorie deficit + daily steps target.")
 elif "muscle" in goal or "gain" in goal or "bulk" in goal:
     print("Plan: Strength training + high protein diet.")
+
+
+    
 elif "abs" in goal or "core" in goal:
     print("Plan: Core workout + calorie control + HIIT.")
 elif "fit" in goal or "healthy" in goal:
