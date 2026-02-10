9faaa940070ed64de67d73596d5fdb21e4a068fb
adithyagrow1 <adithya030105@outlook.com>
2026-02-10 21:54:33 +0530

    D22.py

--- Day22_3.py
+++ Day22_3.py
@@ -3,7 +3,7 @@ email = input("Enter the email message: ").lower()
 if any(word in email for word in ["meeting", "project", "deadline", "team"]):
     print("Category: 🧑‍💼 Work Email")
 
-    
+
 elif any(word in email for word in ["mom", "dad", "friend", "family"]):
     print("Category: ❤️ Personal Email")
 
@@ -14,7 +14,5 @@ elif any(word in email for word in ["offer", "sale", "discount", "deal"]):
 
 elif any(word in email for word in ["lottery", "free money", "click link", "urgent"]):
     print("Category: 🚨 Spam Email")
-
-
 else:
     print("Category: 📩 General Email (No clear category)")
