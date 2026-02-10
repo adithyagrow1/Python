bffae71becc481aeddf8ae0740bcbfca1f7bef8c
adithyagrow1 <adithya030105@outlook.com>
2026-02-10 21:49:55 +0530

    D22.py

--- Day22_2.py
+++ Day22_2.py
@@ -10,8 +10,9 @@ elif "muscle" in goal or "gain" in goal or "bulk" in goal:
 
 elif "abs" in goal or "core" in goal:
     print("Plan: Core workout + calorie control + HIIT.")
-    
+
 elif "fit" in goal or "healthy" in goal:
     print("Plan: Balanced routine with cardio + strength + stretching.")
+    
 else:
     print("Goal unclear. Please specify weight loss, muscle gain, or general fitness.")
