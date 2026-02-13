526d32edae11f5dfc660a14b97963854dae7f16e
adithyagrow1 <adithya030105@outlook.com>
2026-02-13 22:29:02 +0530

    D20.py

--- Day20_2.py
+++ Day20_2.py
@@ -3,10 +3,13 @@ query = input("Describe your issue: ").lower()
 
 if "refund" in query or "money" in query:
     print("→ Refund Team: We will initiate your refund within 2–3 days.")
+    
 elif "delay" in query or "late" in query or "not delivered" in query:
     print("→ Delivery Team: Your order is delayed. We are checking the status.")
+
 elif "broken" in query or "damaged" in query:
     print("→ Replacement Team: We will arrange a replacement soon.")
+
 elif "cancel" in query:
     print("→ Cancellation Team: Your order will be cancelled.")
 
