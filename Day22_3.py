b687056f13e4032d2734d1c7a47915314b3f6ee1
adithyagrow1 <adithya030105@outlook.com>
2026-02-15 22:30:15 +0530

    D22.py

--- Day22_3.py
+++ Day22_3.py
@@ -15,6 +15,7 @@ elif any(word in email for word in ["offer", "sale", "discount", "deal"]):
     print("Category: 🛍 Promotion Email")
 
 
+
 elif any(word in email for word in ["lottery", "free money", "click link", "urgent"]):
     print("Category: 🚨 Spam Email")
 else:
