039a9366deb7f6ca806a8e19dc837757637b6d51
adithyagrow1 <adithya030105@outlook.com>
2026-02-16 22:04:26 +0530

    D22,py

--- Day22_1.py
+++ Day22_1.py
@@ -7,6 +7,7 @@ if any(word in email for word in ["meeting", "project", "deadline", "team"]):
 elif any(word in email for word in ["mom", "dad", "friend", "family"]):
     print("Category: ❤️ Personal Email")
 
+
 elif any(word in email for word in ["offer", "sale", "discount", "deal"]):
     print("Category: 🛍 Promotion Email")
 
