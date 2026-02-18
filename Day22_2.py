02cde2450a458b795d4470866702f553c1e5f580
adithyagrow1 <adithya030105@outlook.com>
2026-02-18 22:03:59 +0530

    D22.py

--- Day22_2.py
+++ Day22_2.py
@@ -1,5 +1,4 @@
 goal = input("What is your fitness goal? ").lower()
-
 if "weight loss" in goal or "lose weight" in goal:
     print("Plan: Cardio + calorie deficit + daily steps target.")
 elif "muscle" in goal or "gain" in goal or "bulk" in goal:
