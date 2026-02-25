c05679900a4c3c86817230a596ed735995549d0a
adithyagrow1 <adithya030105@outlook.com>
2026-02-25 23:12:23 +0530

    D22.py

--- Day22_1.py
+++ Day22_1.py
@@ -12,6 +12,7 @@ if any(word in email for word in ["meeting", "project", "deadline", "team"]):
 
 
 
+
 elif any(word in email for word in ["mom", "dad", "friend", "family"]):
     print("Category: ❤️ Personal Email")
 
