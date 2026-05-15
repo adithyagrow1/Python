d956da66f36d51de20facbc35583067e4a932e14
adithyagrow1 <adithya030105@outlook.com>
2026-05-15 23:05:43 +0530

    D22.py

--- Day22_3.py
+++ Day22_3.py
@@ -6,6 +6,7 @@ email = input("Enter the email message: ").lower()
 if any(word in email for word in ["meeting", "project", "deadline", "team"]):
     print("Category: 🧑‍💼 Work Email")
 
+
 elif any(word in email for word in ["mom", "dad", "friend", "family"]):
     print("Category: ❤️ Personal Email")
 
