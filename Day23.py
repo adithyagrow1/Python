5aede9d416f5937092beba818d8da987a45d2305
Adithya <adithya.ms030105@gmail.com>
2026-06-05 20:24:31 +0530

    D23

--- Day23.py
+++ Day23.py
@@ -3,13 +3,11 @@
 #Area of a square
 side = float(input("enter the length of the side:"))
 print("The area of the square is:",side**2)
-
 #to get the average of two numbers
 a = int(input("enter first number:"))
 b = int(input("enter second number:"))
 c = a+b
 print("average of two numbers is:", c/2)
-
 #true is a>=b if not print false
 a = int(input("enter first number:"))
 b = int(input("enter second number:"))
