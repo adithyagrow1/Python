516eb79811d3d537dfa5df2171326943207af01a
Adithya <165917425+adithyagrow1@users.noreply.github.com>
2026-06-06 20:47:55 +0530

    D24.py

---
+++ Day24.py
@@ -0,0 +1,18 @@
+#outputs the length of the word
+name = input("enter your first name:")
+print(len(name))
+#find the occurrence of "$" in a string
+str1 = "a$d$ithya$"
+print(str4.count("$"))
+#endswith("")
+str0 = "i am adithya"
+print(str0.endswith("ya"))
+#capitalize
+str1 = "i am adithya"
+print(str1.capitalize())
+#replace
+str2 = "i am adithya"
+print(str2.replace("a","@"))
+#find
+str3 = "i am adithya"
+print(str3.find("a"))
